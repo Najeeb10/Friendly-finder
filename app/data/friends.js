2a8aa751e78a0329b5606f365f51dc6270583572
Najeeb Sabour <najeeb@Najeebs-MacBook-Pro.local>
2018-02-26 20:01:39 -0600

    files

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+  {
+    name: 'Zulu',
+    profilePic: 'https://i.huffpost.com/gen/3753966/thumbs/s-DOG-EATING-GRASS-large640.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
